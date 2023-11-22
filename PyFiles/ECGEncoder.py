64fb3676fc7ef15c8b13f6621328f31436891196
naavie <84877044+naavie@users.noreply.github.com>
2023-11-22 18:22:32 -0500

    .

--- PyFiles/ECGEncoder.py
+++ PyFiles/ECGEncoder.py
@@ -1,9 +1,10 @@
 class ECGEncoder(OneDimCNN):
     def __init__(self, num_classes):
         super(ECGEncoder, self).__init__(num_classes)
-        self.fc3 = nn.Linear(128, 768)  # Add an additional fully connected layer
 
-    def forward(self, x):
-        x = super().forward(x)
-        x = self.fc3(x)  # Pass the output through the additional fully connected layer
-        return x
+    def encode(self, signal):
+        # Encode the signal using the OneDimCNN model.
+        encoded_signal = self.forward(signal)
+
+        # Return the encoded signal.
+        return encoded_signal
--- PyFiles/model.py
+++ PyFiles/model.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 
-class OneDimCNN(nn.Module):
+class OneDimCNNV1(nn.Module):
     def __init__(self, num_classes):
         super(OneDimCNN, self).__init__()
 
@@ -31,13 +31,43 @@ class OneDimCNN(nn.Module):
         self.relu4 = nn.ReLU()
         self.pool4 = nn.AvgPool1d(kernel_size=2, stride=2)
 
+        # Layer 5
+        self.conv5 = nn.Conv1d(in_channels=256, out_channels=512, kernel_size=3, stride=1, padding=1)
+        self.bn5 = nn.BatchNorm1d(512)
+        self.relu5 = nn.ReLU()
+        self.pool5 = nn.AvgPool1d(kernel_size=2, stride=2)
+
+        # Layer 6
+        self.conv6 = nn.Conv1d(in_channels=512, out_channels=1024, kernel_size=3, stride=1, padding=1)
+        self.bn6 = nn.BatchNorm1d(1024)
+        self.relu6 = nn.ReLU()
+        self.pool6 = nn.AvgPool1d(kernel_size=2, stride=2)
+
+        # Layer 7
+        self.conv7 = nn.Conv1d(in_channels=1024, out_channels=2048, kernel_size=3, stride=1, padding=1)
+        self.bn7 = nn.BatchNorm1d(2048)
+        self.relu7 = nn.ReLU()
+        self.pool7 = nn.AvgPool1d(kernel_size=2, stride=2)
+
+        # Layer 8
+        self.conv8 = nn.Conv1d(in_channels=2048, out_channels=4096, kernel_size=3, stride=1, padding=1)
+        self.bn8 = nn.BatchNorm1d(4096)
+        self.relu8 = nn.ReLU()
+        self.pool8 = nn.AvgPool1d(kernel_size=2, stride=2)
+
+        # Layer 9
+        self.conv9 = nn.Conv1d(in_channels=4096, out_channels=8192, kernel_size=3, stride=1, padding=1)
+        self.bn9 = nn.BatchNorm1d(8192)
+        self.relu9 = nn.ReLU()
+        self.pool9 = nn.AvgPool1d(kernel_size=2, stride=2)      
+
         # Fully Connected Layer 1
         self.fc1 = nn.Linear(79872, 128)
         self.relu5 = nn.ReLU()
         self.dropout1 = nn.Dropout(0.5)
 
         # Fully Connected Layer 2
-        self.fc2 = nn.Linear(128, 128)
+        self.fc2 = nn.Linear(128, num_classes)
 
     def forward(self, x):
         # Layer 1
@@ -45,41 +75,200 @@ class OneDimCNN(nn.Module):
         x = self.bn1(x)
         x = self.relu1(x)
         x = self.pool1(x)
-        print("After Layer 1:", x.size())
 
         # Layer 2
         x = self.conv2(x)
         x = self.bn2(x)
         x = self.relu2(x)
         x = self.pool2(x)
-        print("After Layer 2:", x.size())
 
         # Layer 3
         x = self.conv3(x)
         x = self.bn3(x)
         x = self.relu3(x)
         x = self.pool3(x)
-        print("After Layer 3:", x.size())
 
         # Layer 4
         x = self.conv4(x)
         x = self.bn4(x)
         x = self.relu4(x)
         x = self.pool4(x)
-        print("After Layer 4:", x.size())
 
-        # Flatten the tensor
+        # Layer 5
+        x = self.conv5(x)
+        x = self.bn5(x)
+        x = self.relu5(x)
+        x = self.pool5(x)
+
+        # Layer 6
+        x = self.conv6(x)
+        x = self.bn6(x)
+        x = self.relu6(x)
+        x = self.pool6(x)
+
+        # Layer 7
+        x = self.conv7(x)
+        x = self.bn7(x)
+        x = self.relu7(x)
+        x = self.pool7(x)
+
+        # Layer 8
+        x = self.conv8(x)
+        x = self.bn8(x)
+        x = self.relu8(x)
+        x = self.pool8(x)
+
+        # Layer 9
+        x = self.conv9(x)
+        x = self.bn9(x)
+        x = self.relu9(x)
         x = x.view(x.size(0), -1)
-        print("After Flattening:", x.size())
+
+        # Dynamically adjust the input size of the first fully connected layer
+        if self.fc1.in_features != x.size(1):
+            self.fc1 = nn.Linear(x.size(1), 128)
 
         # Fully Connected Layer 1
         x = self.fc1(x)
         x = self.relu5(x)
         x = self.dropout1(x)
-        print("After FC Layer 1:", x.size())
 
         # Fully Connected Layer 2
         x = self.fc2(x)
-        print("After FC Layer 2:", x.size())
+
+        return x
+
+class OneDimCNNV2(nn.Module):
+    def __init__(self, num_classes):
+        super(OneDimCNN, self).__init__()
+
+        # Layer 1
+        self.conv1 = nn.Conv1d(in_channels=3, out_channels=16, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm1d(16)
+        self.relu1 = nn.ReLU()
+        self.pool1 = nn.AvgPool1d(kernel_size=2, stride=2)
+
+        # Layer 2
+        self.conv2 = nn.Conv1d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm1d(32)
+        self.relu2 = nn.ReLU()
+        self.pool2 = nn.AvgPool1d(kernel_size=2, stride=2)
+
+        # Layer 3
+        self.conv3 = nn.Conv1d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)
+        self.bn3 = nn.BatchNorm1d(64)
+        self.relu3 = nn.ReLU()
+        self.pool3 = nn.AvgPool1d(kernel_size=2, stride=2)
+
+        # Layer 4
+        self.conv4 = nn.Conv1d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1)
+        self.bn4 = nn.BatchNorm1d(128)
+        self.relu4 = nn.ReLU()
+        self.pool4 = nn.AvgPool1d(kernel_size=2, stride=2)
+
+        # Layer 5
+        self.conv5 = nn.Conv1d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1)
+        self.bn5 = nn.BatchNorm1d(256)
+        self.relu5 = nn.ReLU()
+        self.pool5 = nn.AvgPool1d(kernel_size=2, stride=2)
+
+        # Layer 6
+        self.conv6 = nn.Conv1d(in_channels=256, out_channels=512, kernel_size=3, stride=1, padding=1)
+        self.bn6 = nn.BatchNorm1d(512)
+        self.relu6 = nn.ReLU()
+        self.pool6 = nn.AvgPool1d(kernel_size=2, stride=2)
+
+        # Layer 7
+        self.conv7 = nn.Conv1d(in_channels=512, out_channels=1024, kernel_size=3, stride=1, padding=1)
+        self.bn7 = nn.BatchNorm1d(1024)
+        self.relu7 = nn.ReLU()
+        self.pool7 = nn.AvgPool1d(kernel_size=2, stride=2)
+
+        # Layer 8
+        self.conv8 = nn.Conv1d(in_channels=1024, out_channels=2048, kernel_size=3, stride=1, padding=1)
+        self.bn8 = nn.BatchNorm1d(2048)
+        self.relu8 = nn.ReLU()
+        self.pool8 = nn.AvgPool1d(kernel_size=2, stride=2)
+
+        # Layer 9
+        self.conv9 = nn.Conv1d(in_channels=2048, out_channels=4096, kernel_size=3, stride=1, padding=1)
+        self.bn9 = nn.BatchNorm1d(4096)
+        self.relu9 = nn.ReLU()
+        self.pool9 = nn.AvgPool1d(kernel_size=2, stride=2)
+
+        # Fully Connected Layer 1
+        self.fc1 = nn.Linear(4096, 128)  # Adjusted to match output channels of last conv layer
+        self.relu10 = nn.ReLU()
+        self.dropout1 = nn.Dropout(0.5)
+
+        # Fully Connected Layer 2
+        self.fc2 = nn.Linear(128, num_classes)
+
+    def forward(self, x):
+        # Layer 1
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu1(x)
+        x = self.pool1(x)     
+
+        # Layer 2
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu2(x)
+        x = self.pool2(x)
+
+        # Layer 3
+        x = self.conv3(x)
+        x = self.bn3(x)
+        x = self.relu3(x)
+        x = self.pool3(x)
+
+        # Layer 4
+        x = self.conv4(x)
+        x = self.bn4(x)
+        x = self.relu4(x)
+        x = self.pool4(x)
+
+        # Layer 5
+        x = self.conv5(x)
+        x = self.bn5(x)
+        x = self.relu5(x)
+        x = self.pool5(x)
+
+        # Layer 6
+        x = self.conv6(x)
+        x = self.bn6(x)
+        x = self.relu6(x)
+        x = self.pool6(x)
+
+        # Layer 7
+        x = self.conv7(x)
+        x = self.bn7(x)
+        x = self.relu7(x)
+        x = self.pool7(x)
+
+        # Layer 8
+        x = self.conv8(x)
+        x = self.bn8(x)
+        x = self.relu8(x)
+        x = self.pool8(x)
+
+        # Layer 9
+        x = self.conv9(x)
+        x = self.bn9(x)
+        x = self.relu9(x)
+        x = x.view(x.size(0), -1)   
+
+        # Dynamically adjust the input size of the first fully connected layer
+        if self.fc1.in_features != x.size(1):
+            self.fc1 = nn.Linear(x.size(1), 128)
+
+        # Fully Connected Layer 1
+        x = self.fc1(x)
+        x = self.relu10(x)  # Renamed from relu5 to relu10
+        x = self.dropout1(x)
+
+        # Fully Connected Layer 2
+        x = self.fc2(x)
 
         return x
