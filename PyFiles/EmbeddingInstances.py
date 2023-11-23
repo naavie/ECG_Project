98c967dd829ea3a5e1beeeda46315fd94faa54ee
naavie <84877044+naavie@users.noreply.github.com>
2023-11-22 19:18:16 -0500

    .

--- PyFiles/EmbeddingInstances.py
+++ PyFiles/EmbeddingInstances.py
@@ -11,31 +11,32 @@ class InstanceSelector:
         self.text_encoder = text_encoder
         self.ecg_encoder = ecg_encoder
 
-    def get_positive_instances(self):
-        positive_instances = []
-        for i in range(len(self.train_set)):
-            # Generate ECG embedding for the current instance in the training set
-            ecg_embedding = self.ecg_encoder.encode(self.train_set[i][1]['val'])
-            # Generate dx_modality embedding for the current instance in the processed DataFrame
-            dx_modality_embedding = self.text_encoder.encode(self.processed_train_df['dx_modality'][i])
-            # If the ECG embedding and dx_modality embedding are equal, append them as a positive instance
-            if torch.all(torch.eq(ecg_embedding, dx_modality_embedding)):
-                positive_instances.append((ecg_embedding, dx_modality_embedding))
-        return positive_instances
-
-    def get_negative_instances(self):
+    def get_negative_instances(self, current_set, current_set_df, num_neg_instances=5):
+        # Initialize an empty list to store the negative instances
         negative_instances = []
-        # Get the positive instances
-        positive_instances = self.get_positive_instances()
-        for i in range(len(self.train_set)):
-            # Generate ECG embedding for the current instance in the training set
-            ecg_embedding = self.ecg_encoder.encode(self.train_set[i][1]['val'])
-            for j in range(len(self.processed_train_df)):
-                # Only consider dx_modality embeddings that are not at the same index as the current ECG embedding
+
+        # Loop over each record in the current_set
+        for i in tqdm(range(len(current_set)), desc="Generating negative instances"):
+            # Convert the 'val' field of the current record to a PyTorch tensor and store it in the 'signal' variable
+            signal = torch.from_numpy(current_set[i][1]['val']).float()
+
+            # Add an extra dimension to 'signal' to represent the channels
+            signal = signal.unsqueeze(0)
+
+            # Encode the 'signal' using the ECG encoder and store the result in the 'ecg_embedding' variable
+            ecg_embedding = self.ecg_encoder.encode(signal)
+
+            # Randomly select 'num_neg_instances' indices from current_set_df
+            for j in random.sample(range(len(current_set_df)), num_neg_instances):
+                # Check if the current index is not the same as the outer loop index
                 if i != j:
-                    # Generate dx_modality embedding for the current instance in the processed DataFrame
-                    dx_modality_embedding = self.text_encoder.encode(self.processed_train_df['dx_modality'][j])
-                    # If the ECG embedding does not match any of the positive instance embeddings, append it as a negative instance
-                    if not any(torch.all(torch.eq(ecg_embedding, pos[1])) for pos in positive_instances):
+                    # Encode the 'dx_modality' field of the selected record using the text encoder and store the result in the 'dx_modality_embedding' variable
+                    dx_modality_embedding = self.text_encoder.encode(current_set_df['dx_modality'][j])
+
+                    # Check if the ECG embedding is not equal to the DX modality embedding
+                    if not torch.all(torch.eq(ecg_embedding.float(), dx_modality_embedding)):
+                        # If they are not equal, append the pair (ECG embedding, DX modality embedding) to the 'negative_instances' list
                         negative_instances.append((ecg_embedding, dx_modality_embedding))
+
+        # Return the list of negative instances
         return negative_instances
