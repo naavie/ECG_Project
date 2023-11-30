102d5ad875ee104098fd3dedf508ff7990a1fd59
naavie <84877044+naavie@users.noreply.github.com>
2023-11-29 20:38:53 -0500

    .

--- PyFiles/TextEncoders.py
+++ PyFiles/TextEncoders.py
@@ -36,3 +36,34 @@ class TextEncoderV2:
             embeddings = self.model(**inputs).last_hidden_state
         embeddings = torch.mean(embeddings, dim=1)
         return embeddings
+
+
+# Case 3: Same as Case 1, except for Colab GPU 
+
+class TextEncoder(nn.Module):
+    def __init__(self, device):
+        super().__init__()
+        self.device = device
+        self.tokenizer = AutoTokenizer.from_pretrained("emilyalsentzer/Bio_ClinicalBERT")
+        self.model = AutoModel.from_pretrained("emilyalsentzer/Bio_ClinicalBERT").to(self.device)
+
+        # Add embed_dim attribute
+        self.embed_dim = self.model.config.hidden_size
+
+
+    def encode(self, text_list):
+        # Check if text_list is a string representation of a list
+        if isinstance(text_list, str):
+            text_list = ast.literal_eval(text_list)
+        # Convert list of strings to a single string
+        text = ', '.join(text_list)
+        # Tokenize text
+        inputs = self.tokenizer(text, return_tensors="pt", truncation=True, padding=True, max_length=512)
+        # Move inputs to the correct device
+        inputs = {name: tensor.to(self.device) for name, tensor in inputs.items()}
+        # Get embeddings from ClinicalBERT model
+        with torch.no_grad():
+            embeddings = self.model(**inputs).last_hidden_state
+        # Average the embeddings to get single vector per each input
+        embeddings = torch.mean(embeddings, dim=1)
+        return embeddings.to(self.device)
